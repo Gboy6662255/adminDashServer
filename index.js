ec8022499742730a63dbff08ce33412e63139930
Akhil Anand SM <akhil.anand@ilearningengines.com>
2023-02-24 15:44:55 +0530

    ServerConnectioninitialCommit

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const express =require('express')
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const data = require('./data/mydata.json')
+app.use(cors()); 
+app.use(bodyParser.json());
+app.post('/load',(req,res)=>{
+    res.status(200).send(data);
+})
+app.listen(8081,()=>{
+    console.log("Server listening on P 8081...");
+})
