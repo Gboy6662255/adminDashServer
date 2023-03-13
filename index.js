6ff8af619134d8065b12671fa55b1469561a9858
Akhil Anand SM <akhil.anand@ilearningengines.com>
2023-03-13 17:30:37 +0530

    dataSendtoClient

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const data = require('./data/mydata.json')
-const { connectDB, active, getUsers } = require('./src/fetchMongo/MongoFetchData');
+const { connectDB, getUsers } = require('./src/fetchMongo/MongoFetchData');
 
 app.use(cors()); 
 app.use(bodyParser.json());
 connectDB();
-app.post('/load',(req,res)=>{
-    getUsers();
-    res.status(200).send(data);
+app.post('/load',async(req,res)=>{
+    try {
+        const result = await getUsers();
+        res.status(200).send(result);
+      } catch (err) {
+        console.error("Error getting aggregation result:", err);
+        res.status(500).send("Error getting aggregation result");
+      }
 })
 app.listen(8081,()=>{
     console.log("Server listening on P 8081...");
